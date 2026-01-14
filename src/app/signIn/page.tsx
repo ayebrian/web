c2462bcacb0af5378dbcab27f78d72ec08ff0119
Viktor Varenik <yavarenikya@gmail.com>
2026-01-14 19:45:36 +0200

    chore: basic registering feature

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -3,16 +3,37 @@
 import {FriendlyClient, FriendlyClientImpl} from '@/network/friendly-client';
 import {setCookie} from '@/lib/cookies';
 import {Button} from '@/components/ui/button';
+import {useState} from 'react';
+import {Input} from '@/components/ui/input';
+import {
+    Item,
+    ItemContent,
+    ItemDescription,
+    ItemTitle,
+} from '@/components/ui/item';
 
 const client: FriendlyClient = new FriendlyClientImpl();
 
+interface PageState {
+    nickname: string;
+    description: string;
+    interests: string;
+}
+
 export default function SignInPage() {
+    const [state, setState] = useState<PageState>({
+        nickname: '',
+        description: '',
+        interests: '',
+    });
+
     const registerAccount = async () => {
         const auth = await client.generateAccount({
-            nickname: 'kotleni',
-            description:
-                '23 y.o. dude, programmer, linux user, engineer and sometimes - human.',
-            interests: ['typescript', 'kotlin', 'linux', 'cats', 'catgirls'],
+            nickname: state.nickname,
+            description: state.description,
+            interests: state.interests
+                .split(',')
+                .map(interest => interest.trim()),
             avatar: null,
             socialLink: null,
         });
@@ -28,14 +49,49 @@ export default function SignInPage() {
     };
 
     return (
-        <div className="p-8 flex flex-col">
+        <div className="p-8 md:p-64 flex flex-col gap-4">
+            <Input
+                type="text"
+                placeholder="Nickname"
+                value={state.nickname}
+                onChange={e => setState({...state, nickname: e.target.value})}
+            />
+            <Input
+                type="text"
+                placeholder="Description"
+                value={state.description}
+                onChange={e =>
+                    setState({...state, description: e.target.value})
+                }
+            />
+            <Input
+                type="text"
+                placeholder="Interests (separated by ,)"
+                value={state.interests}
+                onChange={e => setState({...state, interests: e.target.value})}
+            />
+
+            <Item variant="outline">
+                <ItemContent>
+                    <ItemTitle>TODO</ItemTitle>
+                    <ItemDescription>
+                        Avatar and socialLink fields is missing.
+                    </ItemDescription>
+                </ItemContent>
+            </Item>
+
             <Button
                 className="cursor-pointer"
                 onClick={() => {
                     void registerAccount();
                 }}
+                disabled={
+                    state.nickname.length < 3 ||
+                    state.description.length < 3 ||
+                    state.interests.length < 1
+                }
             >
-                Create demo account
+                Create account
             </Button>
         </div>
     );
