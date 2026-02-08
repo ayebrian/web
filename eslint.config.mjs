1eec6ce28f3d7d9b44f3e469b9ec88ab472307d0
Viktor Varenik <yavarenikya@gmail.com>
2026-02-08 21:22:59 +0200

    chore: add next-env file to lint ignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,7 +16,7 @@ const compat = new FlatCompat({
 });
 
 export default defineConfig([
-    globalIgnores(['**/build/', 'test/fixtures/', '**/template/', '.next/']),
+    globalIgnores(['**/build/', 'test/fixtures/', '**/template/', '.next/', 'next-env.d.ts']),
     {
         extends: compat.extends(
             'eslint:recommended',
