62a789c510588c19b6e44f495cc745454f4521de
Viktor Varenik <yavarenikya@gmail.com>
2026-06-28 01:36:57 +0300

    chore(menu): generate menu from items array

--- src/app/menu.tsx
+++ src/app/menu.tsx
@@ -4,6 +4,27 @@ import {User, Newspaper, MessageCircle, BookUser} from 'lucide-react';
 import {useTranslations} from 'use-intl';
 import {cn} from '@/lib/utils';
 import {Link, useLocation} from 'react-router';
+import {ReactNode} from 'react';
+
+interface MenuItem {
+    path: string;
+    title: string;
+    icon: ReactNode;
+    releaseTag?: string;
+}
+
+const MENURAIL_ITEMS: MenuItem[] = [
+    {path: '/', title: 'profile', icon: <User />},
+    {path: '/feed', title: 'feed', icon: <BookUser />},
+    {
+        path: '/community',
+        title: 'community',
+        icon: <Newspaper />,
+        releaseTag: 'Q3',
+    },
+    {path: '/chat', title: 'chat', icon: <MessageCircle />, releaseTag: 'Q4'},
+];
+const MENUBAR_ITEMS: MenuItem[] = MENURAIL_ITEMS.toReversed();
 
 export function MenuRail() {
     const t = useTranslations('menu');
@@ -11,61 +32,26 @@ export function MenuRail() {
 
     return (
         <div className="h-full p-1 flex flex-col gap-1.5 lg:p-4 lg:min-w-55">
-            <Link to="/">
-                <Button
-                    variant="ghost"
-                    className={cn(
-                        'cursor-pointer justify-start w-full',
-                        pathname === '/' &&
-                            'bg-accent text-accent-foreground dark:bg-accent/50',
-                    )}
-                >
-                    <User /> <p className="hidden lg:inline">{t('profile')}</p>
-                </Button>
-            </Link>
-            <Link to="/feed">
-                <Button
-                    variant="ghost"
-                    className={cn(
-                        'cursor-pointer justify-start w-full',
-                        pathname === '/feed' &&
-                            'bg-accent text-accent-foreground dark:bg-accent/50',
-                    )}
-                >
-                    <BookUser />{' '}
-                    <p className="hidden lg:block">{t('feed')}</p>{' '}
-                </Button>
-            </Link>
-            <Link to="/community">tailwind
-
-
-                <Button
-                    variant="ghost"
-                    className={cn(
-                        'cursor-pointer justify-start w-full',
-                        pathname === '/community' &&
-                            'bg-accent text-accent-foreground dark:bg-accent/50',
-                    )}
-                >
-                    <Newspaper />{' '}
-                    <p className="hidden lg:block">{t('community')}</p>{' '}
-                    <Badge variant="secondary">Q3</Badge>
-                </Button>
-            </Link>
-            <Link to="/chat">
-                <Button
-                    variant="ghost"
-                    className={cn(
-                        'cursor-pointer justify-start w-full',
-                        pathname === '/chat' &&
-                            'bg-accent text-accent-foreground dark:bg-accent/50',
-                    )}
-                >
-                    <MessageCircle />{' '}
-                    <p className="hidden lg:block">{t('chat')}</p>{' '}
-                    <Badge variant="secondary">Q4</Badge>
-                </Button>
-            </Link>
+            {MENURAIL_ITEMS.map(item => (
+                <Link key={item.path} to={item.path}>
+                    <Button
+                        variant="ghost"
+                        className={cn(
+                            'cursor-pointer justify-start w-full',
+                            pathname === item.path &&
+                                'bg-accent text-accent-foreground dark:bg-accent/50',
+                        )}
+                    >
+                        {item.icon}{' '}
+                        <p className="hidden lg:block">
+                            {t(item.title as Parameters<typeof t>[0])}
+                        </p>{' '}
+                        <Badge hidden={!item.releaseTag} variant="secondary">
+                            {item.releaseTag}
+                        </Badge>
+                    </Button>
+                </Link>
+            ))}
         </div>
     );
 }
@@ -76,74 +62,39 @@ export function MenuBar() {
 
     return (
         <div className="grid grid-cols-4 gap-2 p-4 w-full max-w-md mx-auto">
-            <Link to="/community" className="min-w-0 w-full">
-                <Button
-                    variant="ghost"
-                    className={cn(
-                        'cursor-pointer w-full h-12 py-1 px-1 text-xs sm:text-sm',
-                        pathname === '/community' &&
-                            'bg-accent text-accent-foreground dark:bg-accent/50',
-                    )}
-                >
-                    <div className="flex flex-col items-center justify-between h-full w-full min-w-0">
-                        <div className="flex items-center justify-center h-5">
-                            <Badge variant="secondary" className="scale-90 origin-center">Q3</Badge>
+            {MENUBAR_ITEMS.map(item => (
+                <Link key={item.path} to={item.path} className="min-w-0 w-full">
+                    <Button
+                        variant="ghost"
+                        className={cn(
+                            'cursor-pointer w-full h-12 py-1 px-1 text-xs sm:text-sm',
+                            pathname === item.path &&
+                                'bg-accent text-accent-foreground dark:bg-accent/50',
+                        )}
+                    >
+                        <div className="flex flex-col items-center justify-between h-full w-full min-w-0">
+                            <div className="flex items-center justify-center h-5">
+                                <Badge
+                                    hidden={!item.releaseTag}
+                                    variant="secondary"
+                                    className="scale-90 origin-center"
+                                >
+                                    {item.releaseTag}
+                                </Badge>
+                                <div
+                                    hidden={!!item.releaseTag}
+                                    className="w-4 h-4"
+                                >
+                                    {item.icon}
+                                </div>
+                            </div>
+                            <span className="truncate w-full text-center leading-none mt-auto">
+                                {t(item.title as Parameters<typeof t>[0])}
+                            </span>
                         </div>
-                        <span className="truncate w-full text-center leading-none mt-auto">{t('community')}</span>
-                    </div>
-                </Button>
-            </Link>
-            <Link to="/chat" className="min-w-0 w-full">
-                <Button
-                    variant="ghost"
-                    className={cn(
-                        'cursor-pointer w-full h-12 py-1 px-1 text-xs sm:text-sm',
-                        pathname === '/chat' &&
-                            'bg-accent text-accent-foreground dark:bg-accent/50',
-                    )}
-                >
-                    <div className="flex flex-col items-center justify-between h-full w-full min-w-0">
-                        <div className="flex items-center justify-center h-5">
-                            <Badge variant="secondary" className="scale-90 origin-center">Q4</Badge>
-                        </div>
-                        <span className="truncate w-full text-center leading-none mt-auto">{t('chat')}</span>
-                    </div>
-                </Button>
-            </Link>
-            <Link to="/feed" className="min-w-0 w-full">
-                <Button
-                    variant="ghost"
-                    className={cn(
-                        'cursor-pointer w-full h-12 py-1 px-1 text-xs sm:text-sm',
-                        pathname === '/feed' &&
-                            'bg-accent text-accent-foreground dark:bg-accent/50',
-                    )}
-                >
-                    <div className="flex flex-col items-center justify-between h-full w-full min-w-0">
-                        <div className="flex items-center justify-center h-5">
-                            <BookUser className="w-4 h-4" />
-                        </div>
-                        <span className="truncate w-full text-center leading-none mt-auto">{t('feed')}</span>
-                    </div>
-                </Button>
-            </Link>
-            <Link to="/" className="min-w-0 w-full">
-                <Button
-                    variant="ghost"
-                    className={cn(
-                        'cursor-pointer w-full h-12 py-1 px-1 text-xs sm:text-sm',
-                        pathname === '/' &&
-                            'bg-accent text-accent-foreground dark:bg-accent/50',
-                    )}
-                >
-                    <div className="flex flex-col items-center justify-between h-full w-full min-w-0">
-                        <div className="flex items-center justify-center h-5">
-                            <User className="w-4 h-4" />
-                        </div>
-                        <span className="truncate w-full text-center leading-none mt-auto">{t('profile')}</span>
-                    </div>
-                </Button>
-            </Link>
+                    </Button>
+                </Link>
+            ))}
         </div>
     );
 }
